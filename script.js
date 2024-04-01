41120fa6af8935815e24167f42c7d59cd613f0a0
Sithum <sithumsandaruwan200123@gmail.com>
2024-04-01 13:00:05 +0530

    js editing

--- script.js
+++ script.js
@@ -9,18 +9,17 @@ function searchWeather() {
 
     const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
+
     fetch(currentWeatherUrl)
-        .then(response => response.json())
+        .then(response => {
+            return response.json()
+        })
         .then(data => {
             displayWeather(data);
         })
         .catch(error => {
             console.error('Error fetching current weather data:', error);
-            alert('Error fetching current weather data.please try again');
         });
-
-    
-
 }
 
 function displayWeather(data) {
@@ -28,21 +27,16 @@ function displayWeather(data) {
     const weatherInfoDiv = document.getElementById('weather-info');
     const weatherIcon = document.getElementById('Weather-icon');
 
-    weatherInfoDiv.innerHTML = '';
-    tempDivInfo.innerHTML = '';
-
     if (data.cod === '404') {
         weatherInfoDiv.innerHTML = `<p>${data.message}</p>` ;
-
     } else {
-
         const cityName = data.name;
-        const temperature = Math.round(data.main.temp - 273.15);
+        const temperature = Math.round(Number(data.main.temp) - 273.15);
         const description = data.weather[0].description;
         const iconCode = data.weather[0].icon;
-        const iconUrl = `https://openwathermap.org/img/wn/${iconCode}@4x.png`;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
 
-        const temperatureHTML = `<p>${temperature}C</p>`;
+        const temperatureHTML = `<p>${temperature}℃</p>`;
 
             const weatherHtml = `
             <p>${cityName}</p>
@@ -53,9 +47,7 @@ function displayWeather(data) {
         weatherIcon.src = iconUrl;
         weatherIcon.alt = description;
 
-        showImage();
-                
-        
+        showImage();                
     }
 }
 
