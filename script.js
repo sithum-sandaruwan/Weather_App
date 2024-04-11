044126cf61bb82ee360d5e0c2d070a1177120d6d
Sithum <sithumsandaruwan200123@gmail.com>
2024-04-11 23:54:55 +0530

    Edit the put humidity

--- script.js
+++ script.js
@@ -1,58 +1,75 @@
-// function searchWeather() {
-//     const apiKey = '5c702073e2b1be5c0e7ffb6630e1fb0b';
-//     const city = document.getElementById('city').value;
 
-//     if (!city) {
-//         alert("Please Enter a Correct City Name");
-//         return;
-//     }
+function searchWeather() {
+    const apiKey = '5c702073e2b1be5c0e7ffb6630e1fb0b';
+    const city = document.getElementById('city').value;
 
-//     const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    if (!city) {
+        alert("Please Enter a Correct City Name");
+        return;
+    }
+
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
 
-//     fetch(currentWeatherUrl)
-//         .then(response => {
-//             return response.json()
-//         })
-//         .then(data => {
-//             displayWeather(data);
-//         })
-//         .catch(error => {
-//             console.error('Error fetching current weather data:', error);
-//         });
-// }
+    fetch(currentWeatherUrl)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            console.error('Error fetching current weather data:', error);
+        });
+}
 
-// function displayWeather(data) {
-//     const tempDivInfo = document.getElementById('temp-div');
-//     const weatherInfoDiv = document.getElementById('weather-info');
-//     const weatherIcon = document.getElementById('Weather-icon');
+function displayWeather(data) {
+    const tempDivInfo = document.getElementById('temp-div');
+    const weatherInfoDiv = document.getElementById('weather-info');
+    const weatherIcon = document.getElementById('Weather-icon');
+    const Humidity = document.getElementById('Humidity-level');
 
-//     if (data.cod === '404') {
-//         weatherInfoDiv.innerHTML = `<p>${data.message}</p>` ;
-//     } else {
-//         const cityName = data.name;
-//         const temperature = Math.round(Number(data.main.temp) - 273.15);
-//         const description = data.weather[0].description;
-//         const iconCode = data.weather[0].icon;
-//         const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+    if (data.cod === '404') {
+        weatherInfoDiv.innerHTML = `<p>${data.message}</p>` ;
+    } else {
+        const cityName = data.name;
+        const temperature = Math.round(Number(data.main.temp) - 273.15);
+        const description = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const Humiditylevel = data.main.humidity;
+        //const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
 
-//         const temperatureHTML = `<p>${temperature}℃</p>`;
+        console.log(Humiditylevel);
 
-//             const weatherHtml = `
-//             <p>${cityName}</p>
-//             <p>${description}</p>`;
+        const temperatureHTML = `<p>${temperature}℃</p>`;
 
-//         tempDivInfo.innerHTML = temperatureHTML;
-//         weatherInfoDiv.innerHTML = weatherHtml;
-//         weatherIcon.src = iconUrl;
-//         weatherIcon.alt = description;
+        const weatherHtml = `
+            <p>${cityName}</p>
+            <p>${description}</p>`;
 
-//         showImage();                
-//     }
-// }
-
-// function showImage() {
+        const HumidityHTML = `<p>${Humiditylevel}%</p>`;
     
-//     const weatherIcon = document.getElementById('Weather-icon');
-//     weatherIcon.style.display='block';
-// }
+        const weatherIcons ={
+            '01d': 'images/Weather-icon01.png'
+        };
+    
+        document.getElementById('WeatherIcon').src = `D:\OneDrive\Desktop\Weather_App\images/${weatherIcons[iconCode]}`;
+       
+
+        tempDivInfo.innerHTML = temperatureHTML;
+        weatherInfoDiv.innerHTML = weatherHtml;
+        Humidity.innerHTML = HumidityHTML;
+        // weatherIcon.src = iconUrl;
+        // weatherIcon.alt = description;
+
+        //showImage();                
+    }
+}
+
+function showImage() {
+
+    
+
+    //const weatherIcon = document.getElementById('Weather-icon');
+    WeatherIcon.style.display='block';
+}
